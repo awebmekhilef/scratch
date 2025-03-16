eee9cbc24e5543526c09b6c1b8a4c90b2045e4de
Aweb Mekhilef <awebmekhilef@gmail.com>
2025-03-16 12:36:53 +1100

    Add login and logout functionality

--- app/models.py
+++ app/models.py
@@ -2,19 +2,32 @@ from datetime import datetime, timezone
 from typing import Optional
 from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, WriteOnlyMapped, mapped_column, relationship
-from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, index=True)
     username: Mapped[str] = mapped_column(String(64), unique=True, index=True)
     password_hash: Mapped[Optional[str]] = mapped_column(String(256))
-    
+
     games: WriteOnlyMapped['Game'] = relationship(back_populates='creator')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'<User \'{self.username}\'>'
+
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
 
 
 class Game(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
-from flask import render_template, redirect, flash, url_for
-from app import app
+from urllib.parse import urlsplit
+from flask import render_template, redirect, flash, url_for, request
+from flask_login import current_user, login_user, logout_user
+from app import app, db
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
@@ -10,8 +13,23 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'User {form.username.data} successfully logged in.')
-        return redirect(url_for('index'))
+        user = db.session.scalar(db.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
