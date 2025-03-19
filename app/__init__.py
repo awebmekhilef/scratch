16ba0cc5ed3a828a245c6a743e87c341a7ce1f13
Aweb Mekhilef <awebmekhilef@gmail.com>
2025-03-19 21:23:24 +1100

    Show create and update timestamps in local time

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
 from firebase_admin import credentials
 import firebase_admin
 
@@ -11,6 +12,8 @@ migrate = Migrate()
 login = LoginManager()
 login.login_view = 'auth.login'
 login.login_message = 'Please login to access this page'
+moment = Moment()
+
 
 def create_app():
     app = Flask(__name__)
@@ -19,6 +22,7 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
+    moment.init_app(app)
 
     cred = credentials.Certificate(app.config['GOOGLE_APPLICATION_CREDENTIALS'])
     firebase_admin.initialize_app(cred, {
@@ -35,5 +39,6 @@ def create_app():
     app.register_blueprint(main_bp)
 
     return app
+
 
 from app import models
