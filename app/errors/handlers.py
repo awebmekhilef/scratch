e556c2c7317035997b6023e6a07087a9ac95cb15
Aweb Mekhilef <awebmekhilef@gmail.com>
2025-03-19 12:43:09 +1100

    Fix error handlers not registering

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -3,12 +3,12 @@ from app import db
 from app.errors import bp
 
 
-@bp.errorhandler(404)
+@bp.app_errorhandler(404)
 def not_found(error):
     return render_template('errors/404.html'), 404
 
 
-@bp.errorhandler(500)
+@bp.app_errorhandler(500)
 def internal_server_error(error):
     db.session.rollback()
     return render_template('errors/500.html'), 500
